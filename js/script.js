70310ae96851759be4dfd2a30aba5d43664b7fd1
MR-SAMUR4I <affauzan411@gmail.com>
2024-09-30 10:50:22 +0700

    1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,36 @@
-// Toogle class active
+// Toogle class active untuk hamburger menu
 // Kalau tadinya ada jadi ngga ada
 
 const navbarNav = document.querySelector('.navbar-nav');
-// Ketika hamburger menu di klik
+// Ketika diluar elemen
 document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// Klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu'); //carikan id hamburger-menu
+// Toogle class active untuk form menu
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+const searchBox = document.querySelector('#search-button');
+
+document.querySelector('#search-button').onclick = (e) => {
+    searchForm.classList.toogle('active');
+    searchBox.focus();
+    e.preventDefault(); //untuk mengemblikan default
+}
+
+
+// Klik diluar elemen
+const hm = document.querySelector('#hamburger-menu'); //carikan id hamburger-menu
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+     if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
+    
 })
 
 
